6bbe57eac19e889fff5fb7aef51eef2dcdb927f3
James Deschard <james.deschard@gmail.com>
2022-10-07 19:29:59 +0200

    updated readme

--- engine/document.py
+++ engine/document.py
@@ -54,9 +54,8 @@ class Tag:
     def remove_tags_from_inner_text(self, inner_text):
         cleaner = re.compile(r'<[^>]+>')
         all_inner_tags = re.findall(cleaner, inner_text)
-        is_nested = len(all_inner_tags) == 2
-        if is_nested:
-            cleaner = re.compile(r'<[^>]+>')
+        is_nested = not len(all_inner_tags) == 2
+        if not is_nested:
             cleaned_string = cleaner.sub('', inner_text).strip()
             return cleaned_string
         
